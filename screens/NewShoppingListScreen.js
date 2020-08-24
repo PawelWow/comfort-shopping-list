6360130a689ecdb32bbb3e608d771c465c0c901f
pawel <pawel@webperfect.pl>
2020-08-24 17:48:56 +0200

    screens basic contents

---
+++ screens/NewShoppingListScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet } from 'react-native';
+
+const NewShoppingListScreen = () => {
+    return (
+        <View><Text>Create new list</Text></View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default NewShoppingListScreen;
---
+++ screens/ShoppingListsOverview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet } from 'react-native';
+
+const ShoppingListsOverview = () => {
+    return (
+        <View><Text>Shopping lists</Text></View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default ShoppingListsOverview;
