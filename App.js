60dcf1ef19acd5ccee94233ce4ba798479cfab0f
pawel <pawel@webperfect.pl>
2020-08-25 22:04:02 +0200

    database initialization

--- App.js
+++ App.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 import AppNavigator from './navigation/AppNavigator';
 
+import { init, init2 } from './helpers/db';
+
+init().then(() => {
+  console.log("Database initialized");
+}).catch(err => {
+  console.log("Database initialization failed.");
+  console.log(err);
+});
+
+
 export default function App() {
   return <AppNavigator />;  
 }
---
+++ helpers/db.js
@@ -0,0 +1,57 @@
+import * as SQLite from 'expo-sqlite';
+
+const db = SQLite.openDatabase('shoppinglists.db');
+
+export const init = () => {
+    const promise = new Promise((resolve, reject) => {
+
+        db.transaction(query => {
+            query.executeSql(
+                'CREATE TABLE IF NOT EXISTS lists (id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL);', 
+                [],
+                () => {
+                    resolve()
+                }, 
+                (_, err) => {
+                    reject(err);
+                }
+            );
+
+            query.executeSql(
+                'CREATE TABLE IF NOT EXISTS times (id INTEGER PRIMARY KEY NOT NULL, list_id  INTEGER NOT NULL, shopping_date TEXT NULL, shopping_time TEXT NULL, creation_datetime TEXT NOT NULL);',
+                [],
+                () => {
+                    resolve()
+                }, 
+                (_, err) => {
+                    reject(err);
+                }
+            );
+
+            query.executeSql(
+                'CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY NOT NULL, list_id   INTEGER NOT NULL, content TEXT NOT NULL);',
+                [],
+                () => {
+                    resolve()
+                }, 
+                (_, err) => {
+                    reject(err);
+                }
+            );
+
+            query.executeSql(
+                'CREATE TABLE IF NOT EXISTS items_done (id INTEGER PRIMARY KEY NOT NULL, item_id INTEGER NOT NULL, list_id INTEGER NOT NULL, is_done TEXT NOT NULL DEFAULT 0);',
+                [],
+                () => {
+                    resolve()
+                }, 
+                (_, err) => {
+                    reject(err);
+                }
+            );
+      
+        });      
+    });
+
+    return promise;
+};
