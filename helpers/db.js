141f0e36fbf8f24fad390b906acd7ed41769fb6f
pawel <pawel@webperfect.pl>
2020-08-27 23:23:54 +0200

    insert query

--- helpers/db.js
+++ helpers/db.js
@@ -33,3 +33,52 @@ export const init = () => {
 
     return promise;
 };
+
+// TODO doczytaj jak z tymi resolvami, czy to czasem nie lepiej dać 1 resolve() na końcu
+
+export const insertList = (title, shopping_date, shopping_time, creation_datetime, items) => {
+    const promise = new Promise((resolve, reject) => {
+        db.transaction(tx => {
+
+            tx.executeSql(
+                'INSERT INTO lists (title, shopping_date, shopping_time, creation_datetime) VALUES (?, ?, ?, ?);', 
+                [title, shopping_date, shopping_time, creation_datetime], 
+                (_, result) => {
+                    resolve(result);
+
+                    const itemsCount = items.length();
+                    if(itemsCount === 0) {
+                        // pusta lista, bez itemów
+                        return;
+                    }
+
+                    // teraz itemy listy
+                    const insertQuery = buildInsertItemsQuery(itemsCount);
+                    tx.executeSql(
+                        insertQuery, 
+                        [items], 
+                        (_, result) => {
+                            resolve(result)
+                        },
+                        (_, err) => {
+                            reject(err);
+                        }
+                        );
+                },
+                (_, err) => {
+                    reject(err);
+                }
+                );
+
+                console.log('data' + listId);
+        });
+    });
+
+    return promise;
+};
+
+// buduje insert query dla pojedynczego wiersza lub wielu
+const buildInsertItemsQuery = itemsCount => {
+    const placeholders = new Array(itemsCount).fill('?').join(', ');
+    return `INSERT INTO items (content) VALUES ${placeholders};`;
+}
