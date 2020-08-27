ce6f799509f4ca9634a23cc4e6055ee9faa680a4
pawel <pawel@webperfect.pl>
2020-08-27 19:53:57 +0200

    simplify db structure

--- helpers/db.js
+++ helpers/db.js
@@ -7,7 +7,7 @@ export const init = () => {
 
         db.transaction(query => {
             query.executeSql(
-                'CREATE TABLE IF NOT EXISTS lists (id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL);', 
+                'CREATE TABLE IF NOT EXISTS lists (id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL, shopping_date TEXT NULL, shopping_time TEXT NULL, creation_datetime TEXT NOT NULL);', 
                 [],
                 () => {
                     resolve()
@@ -18,29 +18,7 @@ export const init = () => {
             );
 
             query.executeSql(
-                'CREATE TABLE IF NOT EXISTS times (id INTEGER PRIMARY KEY NOT NULL, list_id  INTEGER NOT NULL, shopping_date TEXT NULL, shopping_time TEXT NULL, creation_datetime TEXT NOT NULL);',
-                [],
-                () => {
-                    resolve()
-                }, 
-                (_, err) => {
-                    reject(err);
-                }
-            );
-
-            query.executeSql(
-                'CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY NOT NULL, list_id   INTEGER NOT NULL, content TEXT NOT NULL);',
-                [],
-                () => {
-                    resolve()
-                }, 
-                (_, err) => {
-                    reject(err);
-                }
-            );
-
-            query.executeSql(
-                'CREATE TABLE IF NOT EXISTS items_done (id INTEGER PRIMARY KEY NOT NULL, item_id INTEGER NOT NULL, list_id INTEGER NOT NULL, is_done TEXT NOT NULL DEFAULT 0);',
+                'CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY NOT NULL, list_id INTEGER NOT NULL, content TEXT NOT NULL, is_done TEXT NOT NULL DEFAULT 0);',
                 [],
                 () => {
                     resolve()
