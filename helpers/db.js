ca8f7112b54c88b127bd36fc9314039924d74fdf
pawel <pawel@webperfect.pl>
2020-08-28 14:30:44 +0200

    insert, update and delete queries

--- helpers/db.js
+++ helpers/db.js
@@ -34,8 +34,7 @@ export const init = () => {
     return promise;
 };
 
-// TODO doczytaj jak z tymi resolvami, czy to czasem nie lepiej dać 1 resolve() na końcu
-
+// dodaje nową listę z itemami
 export const insertList = (title, shopping_date, shopping_time, creation_datetime, items) => {
     const promise = new Promise((resolve, reject) => {
         db.transaction(tx => {
@@ -46,7 +45,7 @@ export const insertList = (title, shopping_date, shopping_time, creation_datetim
                 (_, result) => {
                     resolve(result);
 
-                    const itemsCount = items.length();
+                    const itemsCount = items.length;
                     if(itemsCount === 0) {
                         // pusta lista, bez itemów
                         return;
@@ -54,7 +53,7 @@ export const insertList = (title, shopping_date, shopping_time, creation_datetim
 
                     // teraz itemy listy
                     const insertQuery = buildInsertItemsQuery(itemsCount);
-                    const insertValues = prepareInsertQueryValues(result.insertId, items);
+                    const insertValues = prepareInsertQueryValues(result.insertId, items, 0);
                     tx.executeSql(
                         insertQuery, 
                         insertValues, 
@@ -76,14 +75,169 @@ export const insertList = (title, shopping_date, shopping_time, creation_datetim
     return promise;
 };
 
-// buduje insert query dla pojedynczego wiersza - głównie chodzi o VALUES (id_listy, w1), (id_listy, w2)
+// dodaje itemy do istniejącej listy
+export const insertItems = (listId, items) => {
+    const promise = new Promise((resolve, reject) => {
+
+        db.transaction(tx => {
+            const insertQuery = buildInsertItemsQuery(items.length);
+            const insertValues = prepareInsertQueryValues(listId, items, 0);
+            tx.executeSql(
+                insertQuery, 
+                insertValues, 
+                (_, result) => {
+                    resolve(result)
+                },
+                (_, err) => {
+                    reject(err);
+                }
+                );
+        });
+
+    });
+
+    return promise;
+}
+
+export const updateList = (listId, newTitle, newShoppingDate, newShoppingTime) => {
+    const promise = new Promise((resolve, reject) => {
+        db.transaction(tx => {
+
+            tx.executeSql(
+                "UPDATE lists SET title='?', shopping_date='?', shopping_time='?' WHERE id=?;", 
+                [newTitle, newShoppingDate, newShoppingTime, listId], 
+                (_, result) => {
+                    resolve(result);
+                },
+                (_, err) => {
+                    reject(err);
+                }
+                );
+        });
+    });
+
+    return promise;
+}
+
+// Będzie raczej rzadko, ale będzie możliwe
+export const updateItemContent = (itemId, newContent) => {
+    const promise = new Promise((resolve, reject) => {
+        db.transaction(tx => {
+
+            tx.executeSql(
+                "UPDATE items SET content='?' WHERE id=?;", 
+                [newContent, itemId], 
+                (_, result) => {
+                    resolve(result);
+                },
+                (_, err) => {
+                    reject(err);
+                }
+                );
+        });
+    });
+
+    return promise;    
+}
+
+// do zapisywania, że item jest zrobiony
+export const updateItemDone = (itemId, isDone) => {
+    const promise = new Promise((resolve, reject) => {
+        db.transaction(tx => {
+
+            tx.executeSql(
+                "UPDATE items SET is_done='?' WHERE id=?;", 
+                [isDone, itemId], 
+                (_, result) => {
+                    resolve(result);
+                },
+                (_, err) => {
+                    reject(err);
+                }
+            );
+        });
+    });
+
+    return promise; 
+}
+
+// usuwa podane listy i wszystkie ich itemy
+export const deleteLists = (listIds) => {
+    const promise = new Promise((resolve, reject) => {
+        db.transaction(tx => {
+
+            const deleteItemsQuery = buildDeleteItemsQuery(listIds.length);
+            tx.executeSql(
+                deleteItemsQuery, 
+                listIds, 
+                (_, result) => {
+                    resolve(result);
+
+                    const deleteListsQuery = buildDeleteListQuery(listIds.length);
+                    tx.executeSql(
+                        deleteListsQuery, 
+                        listIds, 
+                        (_, result) => {
+                            resolve(result);
+                        },
+                        (_, err) => {
+                            reject(err);
+                        }
+                    );
+                    
+                },
+                (_, err) => {
+                    reject(err);
+                }
+            );
+        });
+    });
+
+    return promise; 
+}
+
+// usuwa itemy z podanej listy
+export const deleteItems = listId => {
+    const promise = new Promise((resolve, reject) => {
+        db.transaction(tx => {
+
+            const deleteItemsQuery = buildDeleteItemsQuery(1);
+            tx.executeSql(
+                deleteItemsQuery, 
+                listIds, 
+                (_, result) => {
+                    resolve(result);
+                },
+                (_, err) => {
+                    reject(err);
+                }
+            );
+        });
+    });
+
+    return promise;     
+}
+
+// buduje kwerendę usuwania itemów o podanej ilosći
+const buildDeleteItemsQuery = itemsCount => {
+    const placeholders = new Array(itemsCount).fill('list_id=?').join(' OR ');
+    return `DELETE FROM items WHERE ${placeholders};`;
+}
+
+// buduje kwerendę usuwania list o podanej ilości
+const buildDeleteListQuery = listsCount => {
+    const placeholders = new Array(listsCount).fill('id=?').join(' OR ');
+    return `DELETE FROM lists WHERE ${placeholders};`;
+}
+
+// buduje insert query dla pojedynczego wiersza - głównie chodzi o VALUES (id_listy, w1, is_done), (id_listy, w2, is_done)
 const buildInsertItemsQuery = itemsCount => {
-    const placeholders = new Array(itemsCount).fill('?, ?').join(', ');
-    return `INSERT INTO items (content) VALUES ${placeholders};`;
+    const placeholders = new Array(itemsCount).fill('(?, ?, ?)').join(', ');
+    return `INSERT INTO items (list_id, content, is_done) VALUES ${placeholders};`;
 }
 
 // rozszerza tablicę wartości zapytania o id listy, bo dodajemy wartości w postaci (id_listy, w1), (id_listy, w2)
-const prepareInsertQueryValues = (listId, items) => {
-    const values = items.map(item => [listId, item]);
+const prepareInsertQueryValues = (listId, items, isDone) => {
+    const values = items.map(item => [listId, item, isDone]);
     return values.flat();
 }
