b73b1e2cb75fedbf73f69483e164097f6d84fad1
pawel <pawel@webperfect.pl>
2020-08-30 20:02:37 +0200

    DateTime picker for shopping list

--- components/DateTime.js
+++ components/DateTime.js
@@ -4,26 +4,38 @@ import DateTimePicker from '@react-native-community/datetimepicker';
 
 const DateTime = props => {
     const [isEnabled, setIsEnabled] = useState(false);
-    const [showDatePicker, setShowDatePicker] = useState(false);
+    const [showPicker, setShowPicker] = useState(false);
     const [showTimePicker, setShowTimePicker] = useState(false);
 
-    // będziemy mieć osobno czas i osobno datę
-    const [date, setDate] = useState('');
-    const [hour, setHour] = useState('');
+    const PICKER_DATE = 'date';
+    const PICKER_TIME = 'time';
+    const [pickerMode, setPickerMode] = useState(PICKER_DATE);
+
+    const [shoppingTime, setShoppingTime] = useState(new Date());
 
 
+    const onShowDatePickerPress = () => {
+        setShowPicker(true);
+        setPickerMode(PICKER_DATE);
+    };
+
+    const onShowTimePickerPress = () => {
+        setShowPicker(true);
+        setPickerMode(PICKER_TIME);
+    };
 
     const onDateChange = (event, selectedDate) => {
-        // musiym najpierw schować picker, zanim zmienimy jakąś wartość, inaczej pokaże się 2x
-        // TODO na iOS to chyab nie jest modal, więc trzeba będzie zrobic na 2 sposoby
-        setShowDatePicker(false);
+        // musiym najpierw schować picker, zanim zmienimy jakąś wartość, inaczej pokaże się 2x na Androidzie
+        // Na iOS to nie jest modal, ale powinien się picker gdzieś niżej wysunąć/chować.
+        // TODO to trzeba zobaczyć na iPhone jak to będzie wyglądało - prawodopodobnie będzie badziew
+        setShowPicker(false);
 
         if(selectedDate) {
-            setDate(selectedDate.toLocaleDateString());
+            setShoppingTime(selectedDate);
         }
         
     };
-
+// TODO wyświetlanie godzin trzeba poprawić, bo po co sekundy
     const showDateTimePickerSection = () => {
         if(!isEnabled) {
             return;
@@ -31,19 +43,27 @@ const DateTime = props => {
 
         return (
             <View style={styles.dateTimePickerContainer}>
-                <Text>Date:</Text>
-                <Text onPress={ () => setShowDatePicker(true) }>
-                    {!date ? 'Pick date' : date}
-                </Text> 
+                <View>
+                    <Text>Date:</Text>
+                    <Text onPress={onShowDatePickerPress}>
+                        {!shoppingTime ? 'Pick date' : shoppingTime.toLocaleDateString()}
+                    </Text> 
+                </View>
 
-                {showDatePicker && (
+                <View>
+                    <Text>Hour: </Text>
+                    <Text onPress={onShowTimePickerPress}>
+                        {!shoppingTime ? 'Pick hour' : shoppingTime.toLocaleTimeString(
+                            [], {hour: '2-digit', minute: '2-digit'}
+                        )}
+                    </Text> 
+                </View>
+
+
+                {showPicker && (
                     <DateTimePicker
-                        value={() => { 
-                            return (
-                                !date ? new Date() : new Date(date)
-                            );
-                        }}
-                        mode='date'
+                        value={shoppingTime}
+                        mode={pickerMode}
                         onChange={onDateChange}
                     />
                 )}
